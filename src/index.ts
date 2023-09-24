3c21989af224e5d3df3b5436f3df4ddfb5af65c9
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2023-09-24 17:02:52 +0530

    leanrn enum

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,20 @@ let dataReceived:string | number
 dataReceived = 123 
 dataReceived = "12"
 
+
+// Enum type
+
+export enum Color{
+    Red = 2,
+    Green,
+    Blue
+}
+
+let color:Color = Color.Green
+
+console.log(color)
+console.log(Color)
+
+const k:any = Color[2] // Red
+
+console.log(k);
