f03c46f9e39d91bcea604b50230924b7d858cce4
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2023-09-24 15:34:22 +0530

    manipulate dom

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,14 @@ let age:number = 112
 age = 1
 
 console.log(age)
+
+const hello = document.querySelector('#hello')
+
+hello!.innerHTML = 'hello world 1'
+
+const div = document.querySelector('div')
+const p = document.createElement('p')
+
+p.innerText = 'hello world 2, typeScript'
+
+div!.appendChild(p)
