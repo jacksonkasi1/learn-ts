28dbcc117d1098bc0a7bda6a33c6cd91a2fbcd46
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2023-09-24 16:49:22 +0530

    learn union

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,7 @@
-const id: number = 10;
 
-const user_name: string = "João";
+// union type
+let dataReceived:string | number
 
-const isAvilable: boolean = true;
+dataReceived = 123 
+dataReceived = "12"
 
-let x: any = "teste";
-
-x = 10;
-
-const arr: number[] = [1, 2, 3];
-
-const arr2: Array<number> = [1, 2, 3];
-
-const arr3: string[] = ["1", "2", "3"];
-
-arr3.push("4");
-
-arr2.push(4);
-
-// tuple
-let arr4: [number, string, boolean] = [1, "2", true];
-
-let employee: [string, number][];
-
-employee = [
-  ["João", 10],
-  ["Maria", 20],
-];
